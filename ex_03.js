624fdc2889339b9eb3fa8e2de54244be467c60b5
sarah1reindl <sarah1.reindl@epitech.digital>
2024-10-03 13:51:21 +0200

    ex_03.js

---
+++ ex_03.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded" ,function (){
+    document.getElementById("description").innerHTML = "You’re able to do more with your iPhone than ever before.<br/>So iOS 22 reimagines the most iconic parts of the experience to be even more helpful and personal.<br/>Get yours now !<br/>Now featuring a headphone jack!"
+})
