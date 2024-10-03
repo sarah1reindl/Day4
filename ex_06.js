564a8d3e2c5acb0cc6c0056514a4657f1b96dbbb
sarah1reindl <sarah1.reindl@epitech.digital>
2024-10-03 15:44:30 +0200

    ex_06.js

--- ex_06.js
+++ ex_06.js
@@ -1,1 +1,8 @@
-window.alert("Size matters");
+window.onload = function() {
+    let msgImg = document.querySelector("img"); 
+     if (msgImg) {
+        msgImg.addEventListener("mouseover", function() {
+            alert("Size matters");
+        });
+    }
+};
