8af085b5899186b4dfe7776a99ace7bcc313f2cd
sarah1reindl <sarah1.reindl@epitech.digital>
2024-10-03 14:57:59 +0200

    ex_02.js

--- ex_02.js
+++ ex_02.js
@@ -1,1 +1,6 @@
-document.getElementById("description").innerHTML = "0<br/> 1 <br/>2"
+document.addEventListener("DOMContentLoaded", function() {
+    var para = document.getElementsByTagName('p');
+    for (var i = 0; i < para.length; i++) {
+        para[i].innerHTML = i; 
+    }});
+    
